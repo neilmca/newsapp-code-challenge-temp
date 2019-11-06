f22e65d928bea27d912b6cc59640fde28cc43c20
jodiw <jodi.winters@bbc.co.uk>
2019-11-06 11:54:05 +0000

    Fix feeds so they regularly update

--- jodiSolution/src/server.js
+++ jodiSolution/src/server.js
@@ -1,17 +1,33 @@
+const fetch = require('superagent');
 const feedGenerator = require('./feedGenerator');
 
 const Koa = require('koa');
 const app = new Koa();
 
-const fs = require('fs');
-const newsFeed = JSON.parse(fs.readFileSync('../data-feed.json'));
-const videoFeed = JSON.parse(fs.readFileSync('../video-meta.json'));
+let newsFeed;
+let videoFeed;
 
 app.use((ctx) => {
 	return feedGenerator(newsFeed, videoFeed)
 		.then((result) => { ctx.body = result});
 });
 
-app.listen(3120, () => {
-	console.log('Server listening. CTRL+C to quit');
+function updateFeeds() {
+	return Promise.all([
+		fetch.get('https://raw.githubusercontent.com/neilmca/newsapp-code-challenge-temp/master/data-feed.json'),
+		fetch.get('https://raw.githubusercontent.com/neilmca/newsapp-code-challenge-temp/master/video-meta.json')
+	])
+	.then(([feed, video]) => {
+		newsFeed = JSON.parse(feed.text);
+		videoFeed = JSON.parse(video.text);
+		console.log('Feeds updated');
+	});
+}
+
+updateFeeds()
+.then(() => {
+	app.listen(3120, () => {
+		console.log('Server listening. CTRL+C to quit');
+		setTimeout(updateFeeds, 5000);
+	});
 });
